6a638bc84a245c453d86e0f40375b99b3a63bf05
akankah <36075543+akankah@users.noreply.github.com>
2019-10-02 19:40:00 +0700

    Update a.php

--- a.php
+++ a.php
@@ -1,3 +1,22 @@
- var ip = "<?php echo $_SERVER['REMOTE_ADDR']; ?>";
-    document.write(ip);
-
+(function() {
+  var api = "https://ipinfo.io?callback=?";
+  $("#ip_content").html("Waiting...");
+  $.getJSON( api, { 
+    format: "jsonp"
+  })
+  .done(function( response ) 
+  {
+     var result = ""
+    
+    // show all the props returned
+    for (var prop in response)
+    {
+        result += prop + ": " + response[prop] + "<br>";
+    } 
+  
+    // set the new values 
+    $("#ip_content").html(result);
+         
+      console.log(response);
+  });
+})();
